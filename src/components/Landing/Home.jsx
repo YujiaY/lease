3b745db9899b2e674ab09275122a83d8b5a16211
bywdsg <liuyao928@tom.com>
2019-09-18 00:07:00 +1000

    use flexbox to vertically center

--- src/components/Landing/Home.jsx
+++ src/components/Landing/Home.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
 export default class Home extends React.Component {
-  render(){
+  render() {
     return (
       <div className="home-container">
-      <div className="home__wrapper">
-        <h1 className="home__title">Find Your Dream Hub</h1>
-        <form>
-        <input className="home__input" type="text" name="Search"/>
-        <input className="home__submit" type="submit" value="Search"></input>
-        </form>
+        <div className="home__background">
+          <div>
+            <h1 className="home__title">Find Your Dream Hub</h1>
+            <form>
+              <input className="home__input" type="text" name="Search" />
+              <input className="home__submit" type="submit" value="Search"></input>
+            </form>
+          </div>
         </div>
       </div>
     )
