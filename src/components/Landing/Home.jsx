73ff09a307c7eb5db1be42fd102ff79f64320937
bywdsg <liuyao928@tom.com>
2019-09-17 23:37:41 +1000

    finish homepage

--- src/components/Landing/Home.jsx
+++ src/components/Landing/Home.jsx
@@ -3,8 +3,14 @@ import React from 'react';
 export default class Home extends React.Component {
   render(){
     return (
-      <div>
-        <h1>Home</h1>
+      <div className="home-container">
+      <div className="home__wrapper">
+        <h1 className="home__title">Find Your Dream Hub</h1>
+        <form>
+        <input className="home__input" type="text" name="Search"/>
+        <input className="home__submit" type="submit" value="Search"></input>
+        </form>
+        </div>
       </div>
     )
   }
