f016e629ddd323d53c9afc4d8b337416618bd5c2
bywdsg <liuyao928@tom.com>
2019-09-17 21:08:59 +1000

    lab

--- src/components/App/Footer.jsx
+++ src/components/App/Footer.jsx
@@ -7,26 +7,26 @@ export default class Footer extends React.Component {
   render() {
     return (
       <div className="body__footer">
-      <footer className="footer-container">
-        <ul >
-          <li ><Link to="/about" className="footer__about" >About us</Link></li>
-          <li ><Link to="/contact" >Contact</Link></li>
-        </ul>
-        <ul >
-          <li >
-            <a href="https://twitter.com/?lang=en" target="_blank">
-            <img className="footer__icon" src={twitter} alt="twitter" />
-              Twitter
+        <footer className="footer-container">
+          <ul >
+            <li ><Link to="/about" className="footer__about" >About us</Link></li>
+            <li ><Link to="/contact" >Contact</Link></li>
+          </ul>
+          <ul >
+            <li >
+              <a href="https://twitter.com/?lang=en" target="_blank">
+                <img className="footer__icon" src={twitter} alt="twitter" />
+                Twitter
           </a>
-          </li>
-          <li >
-            <a href="https://www.facebook.com" target="_blank">
-            <img className="footer__icon" src={facebook} alt="facebook" />
-              Facebook
+            </li>
+            <li >
+              <a href="https://www.facebook.com" target="_blank">
+                <img className="footer__icon" src={facebook} alt="facebook" />
+                Facebook
           </a>
-          </li>
-        </ul>
-      </footer>
+            </li>
+          </ul>
+        </footer>
       </div>
     );
   }
